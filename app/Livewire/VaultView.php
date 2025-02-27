acfcf679ee6e317e9c0a09f9a82ca470bba91e66
Nick Dillon <nickhds@gmail.com>
2025-02-26 22:43:55 -0600

    Default value for sort_by

--- app/Livewire/VaultView.php
+++ app/Livewire/VaultView.php
@@ -125,7 +125,7 @@ class VaultView extends Component
                         $query->where('genres', 'LIKE', "%$genre%");
                     }
                 })
-                ->orderBy($this->sort_by, $this->sort_direction)
+                ->orderBy($this->sort_by ?? 'id', $this->sort_direction)
                 ->orderBy('sort')
                 ->get(),
         ]);
